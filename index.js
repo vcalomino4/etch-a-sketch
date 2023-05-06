e84c1260302d9e5d509b63ac4405b19c34e1df99
vcalomi <vcalomino@fi.uba.ar>
2023-05-06 17:05:29 -0300

    etch-a-sketch finished

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 const mainContainer = document.querySelector(".container");
+var gridSquares = 16;
 
-for (i = 0; i < 255; i++) {
-  const div = document.createElement("div");
-  div.classList.add("item");
-  mainContainer.appendChild(div);
+function drawGrid(gridDimensions) {
+  for (i = 0; i < gridDimensions * gridDimensions; i++) {
+    const div = document.createElement("div");
+    let width = 480 / gridDimensions;
+    let height = 480 / gridDimensions;
+    div.style.width = width + "px";
+    div.style.height = height + "px";
+    div.classList.add("item");
+    mainContainer.appendChild(div);
+  }
+}
+drawGrid(gridSquares);
+
+function deleteGrid() {
+  items.forEach((item) => {
+    mainContainer.removeChild(item);
+  });
+}
+var items = document.querySelectorAll(".item");
+addListener(items);
+
+function addListener(items) {
+  items.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.classList.toggle("trail");
+    });
+  });
 }
 
-const items = document.querySelectorAll(".item");
-console.log(items);
-
-items.forEach((item) => {
-  item.addEventListener("mouseover", () => {
-    item.classList.toggle("trail");
-  });
-});
-
-var gridSquares = 0;
-const gridSize = document.querySelector("#grid-size");
-gridSize.addEventListener("click", () => {
+const gridDimensions = document.querySelector("#grid-size");
+gridDimensions.addEventListener("click", () => {
   gridSquares = parseInt(prompt("Enter the grid dimension "));
-  while (gridSquares > 100 || isNaN(gridSquares)) {
+  while (gridSquares > 64 || isNaN(gridSquares)) {
     gridSquares = parseInt(prompt("Enter the grid dimension "));
   }
+  deleteGrid();
+  drawGrid(gridSquares);
+  items = document.querySelectorAll(".item");
+  addListener(items);
 });
